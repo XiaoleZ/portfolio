b6716de2df06e469446a379dbf2430b8afbabbd1
XiaoleZ <xiaole2@ualberta.ca>
2020-06-19 00:05:49 -0700

    Updates

---
+++ precache-manifest.a7e2373ab5ce308220cb0a03ca19f36b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe9de619ca65703aea5",
+    "url": "/static/css/main.ba07425f.chunk.css"
+  },
+  {
+    "revision": "0fe9de619ca65703aea5",
+    "url": "/static/js/main.0fe9de61.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f87ef87ce62dc96eff4a657b1a08db7",
+    "url": "/index.html"
+  }
+];
