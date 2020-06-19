4c0b6026325321d463c85af65f43126d115539bd
Charlotte Zeng <charlotte.zeng@clio.com>
2020-06-18 22:45:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+Reference: A simple [SPA Portfolio template](https://github.com/rbhatia46/React-Portfolio) for developer/designers built with React. Use it to showcase your work, testimonials and other information to clients.
+
+
---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Charlotte Xiaole Zeng",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"xiaolezeng",
+    // "skypeid": "Your skypeid",
+    "roleDescription": " I like teaching and exploring new challenges.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/xiaolezeng/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/XiaoleZ",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/lelecharlotte_/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am a fourth year Computing Science student at the University of Alberta.\
+               I like teaching and exploring new challenges.\
+               I am a Full Stack Web Developer, diving deeper into Machine Learning and Practical Algorithm.",
+    // "address":"India",
+    // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Alberta",
+        "specialization":"B.Sc. Honors in Computing Science",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Clio - Cloud-Based Legal Technology",
+        "CompanyLink":"https://www.clio.com/",
+        "specialization":"Software Developer, Co-op",
+        "MonthOfStart":"May",
+        "YearOfStart":"2020",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2020",
+        "Achievements":"<span>&bull;</span> Business Acceleration Team"
+      },
+      {
+        "CompanyName":"ReviewBoard",
+        "CompanyLink":"https://www.reviewboard.org/",
+        "specialization":"Open Source Developer",
+        "MonthOfStart":"Jan",
+        "YearOfStart":"2020",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2020",
+        "Achievements":"<span>&bull;</span> Added a Unit Test for Djblets in Python and Django during code sprint weekend.<br> \
+        <span>&bull;</span> Worked on Inline Text Bubble project, Review request infobox accessibility project and Add Python version to `rbt --version` output project."
+      },
+      {
+        "CompanyName":"University of Alberta",
+        "CompanyLink":"https://www.ualberta.ca/index.html",
+        "specialization":"Research Assistant",
+        "MonthOfStart":"Jan",
+        "YearOfStart":"2019",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2019",
+        "Achievements":"<span>&bull;</span> Worked on a EPCOR project using a machine learning algorithm (YOLOv3) to classify defects in sewer pipes to reduce inspection time and cost compared with traditional process.<br>\
+        <span>&bull;</span> Conducted numerical experiments with experience in data gathering, image processing, data augmentation, feature extraction, and testing using Python and SQL to enhance accuracy and classification performance.<br>\
+        <span>&bull;</span> Wrote and presented weekly status reports and a progress report in front of 10 people."
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Languages",
+        "skilldetails":"Python, C, Java, JavaScript, HTML/CSS, Ruby, SQL/SQLite, Matlab"
+      },
+      {
+        "skillname":"Frameworks",
+        "skilldetails":"Django, Ruby on Rails, Angular, Bootstrap, Semantic UI, MaterializeCSS, Phaser"
+      },
+      {
+        "skillname":"Frameworks",
+        "skilldetails":"jQuery, Backbone.js"
+      },
+      {
+        "skillname":"Skills",
+        "skilldetails":"Agile, Git, Andriod, Heroku, YOLOv3"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"OverProofed",
+        "description":"A Web video game about sourdough",
+        "imgurl":"images/portfolio/overproofed.png"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"The two most powerful warriors are patience and time.",
+        "name":"Leo Tolstoy"
+      },
+      {
+        "description":"The gladdest moment in human life is a departure into unknown lands.",
+        "name":"Sir Richard Burton"
+      },
+      {
+        "description":"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.",
+        "name":"Pele"
+      }
+    ]
+  }
+  
+  export default resumeData
